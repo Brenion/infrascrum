6d77dd358b4963ca7068fd1c361088963814c3c1
LaynMDream <layn_mdream_@hotmail.com>
2022-07-12 15:05:20 +0200

    update header

--- app/pods/components/header/component.js
+++ app/pods/components/header/component.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+export default class HeaderComponent extends Component {
+  @tracked isVisible = false;
 
-export default class HeaderComponent extends Component {}
+  @action
+  toggleEdit() {
+    this.isVisible = !this.isVisible;
+    console.log(this.isVisible);
+  }
+}
