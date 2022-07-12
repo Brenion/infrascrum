f69bbc05d1dfafa0359178c9ecb9e5a59f1c4981
AndyTierce <andytierce@gmail.com>
2022-07-12 15:16:08 +0200

    test couleurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@
 module.exports = {
   content: ['./app/**/*.hbs'],
   theme: {
+    colors: {
+    'chartreuse' : '#7FFF00',
+    'white' : '#FFFFFF',
+    'black' : '#000000',
+    'middlegray' : '#cacfe0',
+    'lightgray' : '##eaecf3',
+    'lightblue' : '#BAE6FD',
+    'middleblue' : '#0f70b7',
+    'nightblue': '#1E40AF',
+    'darkblue' : '#001626',
+    'red' : '#DC2626',
+    'yellow' : '#FACC15',
+    'orange' : '#F97316',
+    'green' : '#10B981'
+
+},
     extend: {
       spacing: {
         128: '32rem'
