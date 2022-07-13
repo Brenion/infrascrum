4a5174b66e73fe1a850b1b930aa3f49ec54b325d
LaynMDream <layn_mdream_@hotmail.com>
2022-07-13 12:13:59 +0200

    delete route

--- tests/unit/pods/profils/user/route-test.js
+++ tests/unit/pods/profils/user/route-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'infrascrum/tests/helpers';
 
-module('Unit | Route | profils/users', function (hooks) {
+module('Unit | Route | profils/user', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:profils/users');
+    let route = this.owner.lookup('route:profils/user');
     assert.ok(route);
   });
 });
