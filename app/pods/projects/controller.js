55cd92f62f6785552134636e307a768ff36246b4
Urkral <esteban.dutrannois@outlook.com>
2022-07-19 13:42:13 +0200

    ListProject (almost final)

--- app/pods/projects/controller.js
+++ app/pods/projects/controller.js
@@ -1,9 +1,3 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 
-export default class ProjectsController extends Controller {
-  @action deleteProject(project, e) {
-    e.preventDefault();
-    project.destroyRecord();
-  }
-}
+export default class ProjectsController extends Controller {}
