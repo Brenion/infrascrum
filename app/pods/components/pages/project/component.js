5e8050f64064b28f79e5b3308b95ac08bdccca07
LaynMDream <layn_mdream_@hotmail.com>
2022-07-19 11:16:38 +0200

    update

--- app/pods/components/pages/project/component.js
+++ app/pods/components/pages/project/component.js
@@ -1,3 +1,9 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
 
-export default class PagesProjectComponent extends Component {}
+export default class PagesProjectComponent extends Component {
+  @service store;
+  model() {
+    return this.store.findAll('project');
+  }
+}
