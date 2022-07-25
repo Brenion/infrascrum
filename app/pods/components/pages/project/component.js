e07c2a4b826c0db90f01433b89c70414e38d97d6
AndyTierce <andytierce@gmail.com>
2022-07-25 21:47:03 +0200

    Fix du header et navbar + début édit elements

--- app/pods/components/pages/project/component.js
+++ app/pods/components/pages/project/component.js
@@ -1,3 +1,30 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class PagesProjectComponent extends Component {}
+export default class PagesProjectComponent extends Component {
+  @tracked isEditElement = false;
+
+  @tracked selectElement = {};
+  @tracked selectProject = {};
+
+  @action editElement() {
+    this.isEditElement = !this.isEditElement;
+    console.log(this.isEditElement);
+  }
+
+  @action async saveElement(e) {
+    e.preventDefault();
+
+    const setOnProject = await this.store.peekRecord(
+      'project',
+      this.selectProject.id
+    );
+    let rec = this.store.editRecord('element', {
+      nameElement: this.selectElement.nameElement,
+      colorElement: this.selectElement.colorElement,
+      project: setOnProject,
+    });
+    await rec.save();
+  }
+}
