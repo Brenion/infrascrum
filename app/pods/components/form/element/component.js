0553e4ee652f134d03b984a0ddeccaeec572d041
Urkral <esteban.dutrannois@outlook.com>
2022-07-25 12:09:22 +0200

    Design element

--- app/pods/components/form/element/component.js
+++ app/pods/components/form/element/component.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import EmberArray from '@ember/array';
 
 export default class FormElementComponent extends Component {
   @service router;
