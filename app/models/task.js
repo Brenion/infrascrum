8ab50f1978827732d39557159d73b4f389d4b17b
Urkral <esteban.dutrannois@outlook.com>
2022-07-25 18:55:25 +0200

    r

--- app/models/task.js
+++ app/models/task.js
@@ -2,8 +2,8 @@ import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 
 export default class TaskModel extends Model {
   @attr('string') title;
-  @attr('boolean') description;
-  @attr('string') done;
+  @attr('string') description;
+  @attr('boolean') done;
   @attr('string') time;
   @attr('string') colorTask;
   @belongsTo('type') type;
