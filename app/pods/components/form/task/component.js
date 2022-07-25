852cdb4cdda09d5b61517d08671c888f4d31a501
extremile <miltonleguizamon@yahoo.fr>
2022-07-25 13:58:26 +0200

    update task

--- app/pods/components/form/task/component.js
+++ app/pods/components/form/task/component.js
@@ -1,3 +1,21 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class FormTaskComponent extends Component {}
+export default class FormTaskComponent extends Component {
+  @service router;
+  @service store;
+  @tracked selectTask = {};
+  @tracked selectProject = {};
+  @action async saveTask(e) {
+    e.preventDefault();
+
+    let rec = this.store.createRecord('task', {
+      nameTask: this.selectTask.nameTask,
+      colorTask: this.selectTask.colorTask,
+    });
+    await rec.save();
+    //this.router.transitionTo(`${this.selectProject.id}`);
+  }
+}
