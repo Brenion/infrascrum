df1d3ea5ec60d87fdd86497ce1409da4e4d9a9fa
AndyTierce <andytierce@gmail.com>
2022-07-26 11:09:39 +0200

    début du mini crud édition d'element

--- app/pods/components/pages/project/component.js
+++ app/pods/components/pages/project/component.js
@@ -5,26 +5,30 @@ import { action } from '@ember/object';
 export default class PagesProjectComponent extends Component {
   @tracked isEditElement = false;
 
-  @tracked selectElement = {};
-  @tracked selectProject = {};
+  // @tracked selectElement = {};
+  // @tracked selectProject = {};
 
   @action editElement() {
     this.isEditElement = !this.isEditElement;
     console.log(this.isEditElement);
   }
-
-  @action async saveElement(e) {
+  @action deleteElement(element, e) {
     e.preventDefault();
-
-    const setOnProject = await this.store.peekRecord(
-      'project',
-      this.selectProject.id
-    );
-    let rec = this.store.editRecord('element', {
-      nameElement: this.selectElement.nameElement,
-      colorElement: this.selectElement.colorElement,
-      project: setOnProject,
-    });
-    await rec.save();
+    element.destroyRecord();
   }
+
+  // @action async saveElement(e) {
+  //   e.preventDefault();
+
+  //   const setOnProject = await this.store.peekRecord(
+  //     'project',
+  //     this.selectProject.id
+  //   );
+  //   let rec = this.store.setRecord('element', {
+  //     nameElement: this.selectElement.nameElement,
+  //     colorElement: this.selectElement.colorElement,
+  //     project: setOnProject,
+  //   });
+  //   await rec.save();
+  // }
 }
