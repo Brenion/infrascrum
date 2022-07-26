949395b03d722393b913f4640253a32c2bef53af
Urkral <esteban.dutrannois@outlook.com>
2022-07-26 16:15:20 +0200

    .

--- app/pods/projects/id/tasks/id/route.js
+++ app/pods/projects/id/tasks/id/route.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class ProjectsIdTasksIdRoute extends Route {}
+export default class ProjectsIdTasksIdRoute extends Route {
+  @service store;
+  model(params) {
+    return this.store.findRecord('task', params.id);
+  }
+}
