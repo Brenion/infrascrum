8446ab703129f820e395d4b1e1bcb00030ff25f8
LaynMDream <layn_mdream_@hotmail.com>
2022-07-26 16:32:44 +0200

    reseting

--- app/pods/components/form/task/component.js
+++ app/pods/components/form/task/component.js
@@ -1,21 +1,3 @@
 import Component from '@glimmer/component';
-import { service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class FormTaskComponent extends Component {
-  @service router;
-  @service store;
-  @tracked selectTask = {};
-  @tracked selectProject = {};
-  @action async saveTask(e) {
-    e.preventDefault();
-
-    let rec = this.store.createRecord('task', {
-      nameTask: this.selectTask.nameTask,
-      colorTask: this.selectTask.colorTask,
-    });
-    await rec.save();
-    //this.router.transitionTo(`${this.selectProject.id}`);
-  }
-}
+export default class FormTaskComponent extends Component {}
--- app/pods/components/pages/project/component.js
+++ app/pods/components/pages/project/component.js
@@ -1,34 +1,3 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class PagesProjectComponent extends Component {
-  @tracked isEditElement = false;
-
-  // @tracked selectElement = {};
-  // @tracked selectProject = {};
-
-  @action editElement() {
-    this.isEditElement = !this.isEditElement;
-    console.log(this.isEditElement);
-  }
-  @action deleteElement(element, e) {
-    e.preventDefault();
-    element.destroyRecord();
-  }
-
-  // @action async saveElement(e) {
-  //   e.preventDefault();
-
-  //   const setOnProject = await this.store.peekRecord(
-  //     'project',
-  //     this.selectProject.id
-  //   );
-  //   let rec = this.store.setRecord('element', {
-  //     nameElement: this.selectElement.nameElement,
-  //     colorElement: this.selectElement.colorElement,
-  //     project: setOnProject,
-  //   });
-  //   await rec.save();
-  // }
-}
+export default class PagesProjectComponent extends Component {}
