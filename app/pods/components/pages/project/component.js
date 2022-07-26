cdcd04b08b1fcb3936fc6eecc36010d757269f37
Urkral <esteban.dutrannois@outlook.com>
2022-07-26 11:18:50 +0200

    .

--- app/pods/components/pages/project/component.js
+++ app/pods/components/pages/project/component.js
@@ -1,3 +1,17 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class PagesProjectComponent extends Component {}
+export default class PagesProjectComponent extends Component {
+  @action deleteTask(task, e) {
+    e.preventDefault();
+    task.destroyRecord();
+  }
+  @action finishTask(e) {
+    if (e.checked) {
+      e.style.visibility = 'visible';
+      e.value = '';
+    } else {
+      e.style.visibility = 'hidden';
+    }
+  }
+}
