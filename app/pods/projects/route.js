604d4e83c4a09db3d7c7918fcee8738e0529ba0f
LaynMDream <layn_mdream_@hotmail.com>
2022-07-27 11:30:33 +0200

    Debug user

--- app/pods/projects/route.js
+++ app/pods/projects/route.js
@@ -5,7 +5,7 @@ export default class ProjectsRoute extends Route {
   @service user;
   @service store;
   async model() {
-    // await this.user.model();
+    await this.user.model();
     return this.store.findAll('project');
   }
 }
