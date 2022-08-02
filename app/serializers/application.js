66c74b405e7e789b8ffc52a45f15161221df569e
extremile <miltonleguizamon@yahoo.fr>
2022-08-02 15:57:16 +0200

    upvalid

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,3 @@
-import JSONAPISerializer from '@ember-data/serializer/json';
+import JSONAPISerializer from '@ember-data/serializer/rest';
 
 export default class ApplicationSerializer extends JSONAPISerializer {}
